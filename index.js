b63fddd1da2b975fbd47d3b404621fbf8843c86a
mstrYoda <freelancemuhendis@gmail.com>
2016-12-21 21:10:53 +0200

    msngr webhook post - send receive message

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+app.set('jeton','EAAII24KnAZBABAD5oYTZAIoolzWtAvvIDz49QAzgMV5eZBEVRydicvNrVxWHn9lS986W052TSpUVdRToRuZBcLc5MZB9gzr6MO63yVZCZBAXO3BqpiUhgkyuBavrWzDKeZAvNiv35LShCZCupDq77vbW3wKoNbmoJZCoTe5CqP5TnA9gZDZD');
 
 
 app.get('/',function(req,res){
@@ -23,25 +24,109 @@ app.get('/webhook',function(req,res){
     console.error("Failed validation. Make sure the validation tokens match.");
     res.sendStatus(403);          
   }  
-})
+});
 
-function getSuggestion(req,res){
-    if(req.query.token == 12345){
-      request('http://suggestqueries.google.com/complete/search?output=client&client=firefox&hl=tr-TR&q='+req.query.keyword,
-          function(error,response,body){
-            if(!error && response.statusCode == 200){
-              var donen = JSON.parse(body);
-              var str = ''
-              donen[1].forEach(function(entry){
-                str += '<li>'+entry+'</li>';
-              });
-              res.status(200).json(body);
-            }
-          });
 
-    }else{
-      res.send('Hatalı istek');
+
+app.post('/webhook',function(req,res){
+
+  var data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
+    res.sendStatus(200);
+  }
+
+
+
+});
+
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  console.log("Received message for user %d and page %d at %d with message:", 
+    senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var messageId = message.mid;
+
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+
+  if (messageText) {
+
+    // If we receive a text message, check to see if it matches a keyword
+    // and send back the example. Otherwise, just echo the text we received.
+    switch (messageText) {
+      case 'generic':
+        sendGenericMessage(senderID);
+        break;
+
+      default:
+        sendTextMessage(senderID, messageText);
     }
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, "Message with attachment received");
+  }
+}
+
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  };
+
+  callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: app.get('jeton') },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s", 
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });  
 }
 
 app.listen(app.get('port'), function () {
